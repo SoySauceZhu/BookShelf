c8fe25bc4f9e855159e1979b663a04556f5535a4
Mingjie.Zhu22 <Mingjie.Zhu22>
2024-01-31 14:46:41 +0800

    Add new services

--- src/main/java/org/example/officialmysql/controller/BookController.java
+++ src/main/java/org/example/officialmysql/controller/BookController.java
@@ -5,6 +5,8 @@ import org.example.officialmysql.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -16,16 +18,50 @@ public class BookController {
     }
 
     @GetMapping()
-    public @ResponseBody Iterable<Book> getAllBooks() {
+    public Iterable<Book> getAllBooks() {
         return bookService.findAll();
     }
 
     @PostMapping("/add")
-    public @ResponseBody String addNewBook(@RequestParam String title, @RequestParam String author, @RequestParam String isbn) {
+    public String addNewBook(@RequestParam String title, @RequestParam String author, @RequestParam String isbn) {
+        // Restful controller automatically declares as @ResponseBody, to serialize response into json
         Book n = new Book(title, isbn, author);
         bookService.save(n);
         return "\nSuccessfully Saved :\t" + n;
     }
+
+    @PostMapping("/addBook")
+    public String addNewBook(@RequestBody Book book) {
+        // Requires a JSON like post request
+        bookService.save(book);
+        return "\nSuccessfully Saved :\t" + book;
+    }
+
+    @GetMapping("/book")
+    public Optional<Book> findByName(@RequestParam(defaultValue = "") String title, @RequestParam(defaultValue = "") String isbn) {
+        if (title.isEmpty()) {
+            return bookService.findByIsbn(isbn);
+        } else if (isbn.isEmpty()) {
+            return bookService.findByName(title);
+        }
+        return bookService.findByName(title);
+    }
+
+
+    @GetMapping("/book/{isbn}")
+    public Optional<Book> findByIsbn(@PathVariable String isbn) {
+        return bookService.findByIsbn(isbn);
+    }
+
+
+    @DeleteMapping("/deleteAll")
+    public String deleteAllBook() {
+        // Delete all books but the sequence will not be reset
+        bookService.deleteAll();
+        System.out.println("Successfully delete all books");
+        return "Successfully delete all books";
+    }
+
 
 }
 
--- src/main/java/org/example/officialmysql/service/BookRepository.java
+++ src/main/java/org/example/officialmysql/service/BookRepository.java
@@ -3,7 +3,12 @@ package org.example.officialmysql.service;
 import org.example.officialmysql.entities.Book;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends CrudRepository<Book, Long> {
     // Spring automatically implements this repository interface in a bean that has the same name
+
+    Optional<Book> findBookByName(String title);
+    Optional<Book> findBookByIsbn(String isbn);
 
 }
